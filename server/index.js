d357c633d25da8b2f5c933240eebfcf9f5a11a13
greyewi1 <greyewi@gmail.com>
2021-05-22 14:16:16 +0500

    init MVC template (now only routers as view)

--- server/index.js
+++ server/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const connect = require('./database');
-const myConnect = connect
-const {sqlCreateProject, sqlProjects, sqlUpdateProjects, sqlDeleteProject} = require('./dbQueries')
-
 const app = express();
 
-app.listen(8000, () => {
-    console.log("Server is working!!!");
-})
+const projectRouters = require('./routers/projectRouter')
 
 //send data
 app.use(express.json());
@@ -20,30 +13,8 @@ app.use(express.urlencoded({
 
 app.use(bodyParser.json()); //json format
 app.use(cors({origin: "http://localhost:3000"}));
+app.use('/project', projectRouters)
 
-//CREATE endpoint
-
-app.post('/project/', async (req, res) => {
-    const {body: {name, code}} = req
-    const newProject = await myConnect.query(sqlCreateProject, [name, code])
-    res.status(200).send(newProject);
+app.listen(8000, () => {
+    console.log("Server is working!!!");
 })
-
-app.get('/project/', async (req, res) => {
-    const {query: {limit, offset}} = req
-    const projectRead = await myConnect.query(sqlProjects, [limit, offset]);
-    res.status(200).send(projectRead);
-})
-
-app.put('/project/', async(req, res) => {
-    const {body: {id, name, code}} = req
-    const changedProject = await myConnect.query(sqlUpdateProjects, [name, code, id]);
-    res.status(200).send(changedProject);
-})
-
-app.delete('/project/', async(req, res) => {
-    const {body: {id}} = req
-    const deletedProject = await myConnect.query(sqlDeleteProject, [id]);
-    res.status(200).send(deletedProject);
-})
-
