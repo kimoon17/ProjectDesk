eade15e652e9bc10f592f56c041a9f9a9bb1dcaf
kimon986 <kimonmono986@gmail.com>
2021-05-25 14:38:21 -0700

    styling stuff

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,8 +13,13 @@ export function Projects({projectList, fetchProjectList}) {
       <div className="project_boxes">
           {projectList && projectList.map((project) => <div className="project_box" key={project.id}>
             <p className="project_name">{project.name}</p>
+              <p className="project_code">{project.code}</p>
+              <div className="project_task_box">Tasks</div>
         </div>)}
       </div>
+        <div className="button_box">
+            <button className="project_add">+ add</button>
+        </div>
     </div>
   )
 }
